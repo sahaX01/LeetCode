145e775e938c1908873324dd6300873c4e13768a
subham saha <subham.saha.work01@gmail.com>
2026-08-01 13:58:11 +0530

    Time: 3 ms (86.41%), Space: 47.8 MB (16.13%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int candy[] = new int[n];
+       
+        Arrays.fill(candy, 1);
+        
+        // left to right
+        for(int i=1; i<n; i++){
+            if(ratings[i]>ratings[i-1]){
+                candy[i] = candy[i-1] + 1;
+            }
+        }
+        
+        // right to left
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
+                candy[i] = Math.max(candy[i],candy[i+1] + 1);
+            }
+        }
+        int ans = 0;
+        for(int c : candy){
+            ans = ans + c;
+        }
+
+        return ans;
+    }
+}
