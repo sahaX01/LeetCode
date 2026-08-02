eaa759a150abe02334759aa541befd0c681c2878
subham saha <subham.saha.work01@gmail.com>
2026-08-02 11:49:10 +0530

    Time: 0 ms (100%), Space: 42.7 MB (95.35%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,17 +1,5 @@
 class Solution {
-    Integer dp[][];
-
-    public int solve(int[] piles, int i, int j){
-        if(dp[i][j] != null) return dp[i][j];
-        if(i == j) return piles[i];
-        int left = piles[i] - solve(piles, i+1, j);
-        int right = piles[j] - solve(piles, i, j-1);
-        dp[i][j] = Math.max(left, right);
-        return dp[i][j];
-    }
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        dp = new Integer[n][n];
-        return solve(piles, 0, n-1)>=0;
+        return true;
     }
 }
