0013d99921e05a9f1df664a2af2b526cb58c54a0
subham saha <subham.saha.work01@gmail.com>
2026-08-10 21:43:01 +0530

    Time: 37 ms (53.26%), Space: 51 MB (33.68%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,23 +1,36 @@
 class Solution {
-    static Boolean dp[];
-    public static boolean solve(int n){
-        
-        if(n == 0) return false;
-        if(dp[n] != null){
+
+    Boolean[] dp;
+
+    public boolean solve(int n) {
+
+        // No move possible -> current player loses
+        if (n == 0) return false;
+
+        // Already calculated
+        if (dp[n] != null) {
             return dp[n];
         }
-        for(int i=1; i * i<=n; i++){
-          
-          int squre = i * i;
-          if(solve(n-squre) == false){
-            return dp[n] = true;
-          }
+
+        // Try every possible square
+        for (int i = 1; i * i <= n; i++) {
+
+            int square = i * i;
+
+            // after alice move , If Bob no move bob losses
+            // alice wins
+            if (solve(n - square) == false) {
+                return dp[n] = true;
+            }
         }
 
+        // No winning move found for alice
         return dp[n] = false;
     }
+
     public boolean winnerSquareGame(int n) {
-       dp = new Boolean[n+1];
-       return solve(n);
+        dp = new Boolean[n + 1];
+
+        return solve(n);
     }
 }
