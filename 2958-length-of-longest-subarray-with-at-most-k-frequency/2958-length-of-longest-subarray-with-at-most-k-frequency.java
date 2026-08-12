cc5e5555d2ba7562992d134aa119525964c0d22f
subham saha <subham.saha.work01@gmail.com>
2026-08-12 15:16:17 +0530

    Time: 67 ms (59.25%), Space: 88.6 MB (82.34%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -6,13 +6,17 @@ class Solution {
         
         int left = 0;
         for(int right = 0; right<nums.length; right++){
-
+            
+            // Increase frequency of nums[right]
             map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+
+            // If frequency exceeds k, shrink left side window
             while(map.get(nums[right])>k){
             map.put(nums[left], map.get(nums[left])-1);
             left++;
         }
-
+        
+        // Current window length
         ans = Math.max(ans, right + 1 - left); 
         }
 
