1fa7b112749797b8a0d9488b1d86e64d088387a1
subham saha <subham.saha.work01@gmail.com>
2026-08-21 23:06:21 +0530

    Time: 172 ms (11.76%), Space: 43.3 MB (94.12%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
@@ -4,24 +4,35 @@ class Solution {
 
     public long findKthSmallest(int[] coins, int k) {
 
+        // Minimum possible answer
         long low = 1;
+
+        // Maximum answer can be k * smallest coin
         long high = (long) coins[0] * k;
 
+        // Find the smallest coin
         for (int coin : coins) {
             high = Math.min(high, (long) coin * k);
         }
 
+        // Binary search for the kth smallest amount
         while (low < high) {
 
             long mid = low + (high - low) / 2;
 
+            // Count how many valid amounts are <= mid
             if (count(mid, coins) >= k) {
+                // There are already k amounts
+                // Try to find a smaller answer
                 high = mid;
             } else {
+                // Not enough amounts
+                // Need a bigger value
                 low = mid + 1;
             }
         }
 
+        // low is the smallest value having at least k amounts
         return low;
     }
 
@@ -30,22 +41,26 @@ class Solution {
         int n = coins.length;
         long total = 0;
 
-        // Every non-empty subset
+        // Generate every non-empty subset of coins
         for (int mask = 1; mask < (1 << n); mask++) {
 
             long lcm = 1;
             int bits = 0;
             boolean valid = true;
 
+            // Find the LCM of the current subset
             for (int i = 0; i < n; i++) {
 
+                // Check whether coin i is present in this subset
                 if ((mask & (1 << i)) != 0) {
 
                     bits++;
 
+                    // Calculate LCM
                     lcm = lcm(lcm, coins[i]);
 
-                    // No multiple of this LCM can be <= x
+                    // If LCM is already bigger than x,
+                    // no multiple of it can be <= x
                     if (lcm > x) {
                         valid = false;
                         break;
@@ -53,13 +68,21 @@ class Solution {
                 }
             }
 
-            if (!valid) continue;
+            if (!valid) {
+                continue;
+            }
 
+            // Number of multiples of LCM <= x
             long value = x / lcm;
 
+            // Odd number of coins in subset -> add
             if (bits % 2 == 1) {
                 total += value;
-            } else {
+            }
+
+            // Even number of coins -> subtract
+            // This is Inclusion-Exclusion
+            else {
                 total -= value;
             }
         }
@@ -67,6 +90,7 @@ class Solution {
         return total;
     }
 
+    // Calculate GCD using Euclidean algorithm
     private long gcd(long a, long b) {
 
         while (b != 0) {
@@ -78,6 +102,8 @@ class Solution {
         return a;
     }
 
+    // Calculate LCM using:
+    // LCM(a,b) = (a / GCD(a,b)) * b
     private long lcm(long a, long b) {
 
         return a / gcd(a, b) * b;
