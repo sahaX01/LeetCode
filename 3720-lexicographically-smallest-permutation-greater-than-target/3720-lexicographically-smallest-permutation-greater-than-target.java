14eb97683c70d4fbcbfdbca8a0f227495b26d066
subham saha <subham.saha.work01@gmail.com>
2026-08-27 21:34:53 +0530

    Time: 4 ms (38.71%), Space: 44.9 MB (67.74%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
@@ -1,43 +1,74 @@
 class Solution {
     StringBuilder result = new StringBuilder();
-    public  boolean solve(StringBuilder curr, String target, boolean greater, int i, int freq[]){
-        if(i == target.length()){
-            if(greater){
+
+    // Recursive backtracking function
+    public boolean solve(StringBuilder curr, String target,
+                         boolean greater, int i, int freq[]) {
+
+        // If complete permutation is formed
+        if (i == target.length()) {
+
+            // Accept only if permutation is strictly greater than target
+            if (greater) {
                 result = new StringBuilder(curr);
                 return true;
             }
+
             return false;
         }
 
-        for(char ch='a' ;ch<='z'; ch++){
-            if(freq[ch-'a'] == 0) continue;
+        // Try characters from 'a' to 'z'
+        // This ensures lexicographically smallest answer
+        for (char ch = 'a'; ch <= 'z'; ch++) {
 
-            if(greater == false && ch<target.charAt(i)) continue;
+            // Character is not available
+            if (freq[ch - 'a'] == 0)
+                continue;
 
+            // If we are still equal to target,
+            // we cannot choose a smaller character
+            if (!greater && ch < target.charAt(i))
+                continue;
+
+            // Once a greater character is chosen,
+            // the complete string will remain greater
             boolean isGreater = greater || ch > target.charAt(i);
-            curr.append(ch);
-            freq[ch-'a']--;
 
-            if(solve(curr, target, isGreater, i+1, freq)){
+            // Choose current character
+            curr.append(ch);
+            freq[ch - 'a']--;
+
+            // Recursively build the remaining characters
+            if (solve(curr, target, isGreater, i + 1, freq)) {
                 return true;
             }
 
+            // Backtrack
             curr.deleteCharAt(curr.length() - 1);
-            freq[ch-'a']++;
+            freq[ch - 'a']++;
         }
+
         return false;
     }
-    public String lexGreaterPermutation(String s, String target) {
-        int freq[] = new int[26];
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            freq[ch-'a']++;
-        }
-        
-        boolean greater = false;
-        StringBuilder curr = new StringBuilder();
-        solve(curr, target, greater, 0, freq);
 
+    public String lexGreaterPermutation(String s, String target) {
+
+        // Frequency array for characters in s
+        int freq[] = new int[26];
+
+        // Count frequency of each character
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            freq[ch - 'a']++;
+        }
+
+        // Current permutation being built
+        StringBuilder curr = new StringBuilder();
+
+        // Start recursion
+        solve(curr, target, false, 0, freq);
+
+        // Return smallest lexicographically greater permutation
         return result.toString();
     }
 }
