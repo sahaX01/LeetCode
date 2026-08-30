d383417da2be58e1ac6594b711575a817b307adf
subham saha <subham.saha.work01@gmail.com>
2026-08-30 15:24:22 +0530

    Time: 2 ms (100%), Space: 86.7 MB (60.18%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -1,43 +1,38 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
         int n = nums.length;
-        if(n == 1) return 1;
+        if (n == 1)
+            return 1;
 
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
         int mxleft = 0;
         int mxright = 0;
-        int maxIdx = 0;
+        
         int mnleft = 0;
         int mnright = 0;
-        int minIdx = 0;
-        for(int i=0; i<n; i++){
-            if(nums[i]>max){
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > max) {
                 max = nums[i];
-              mxleft = i+1;
-              mxright = n-i;
-              maxIdx = i;
+                mxleft = i + 1;
+                mxright = n - i;
+                
             }
-            if(nums[i]<min){
+            if (nums[i] < min) {
                 min = nums[i];
-                mnleft = i+1;
-                mnright = n-i;
-                minIdx = i;
+                mnleft = i + 1;
+                mnright = n - i;
+               
             }
         }
-        int mid = -1;
-        if(n % 2 == 0){
-           mid = n/2;
-        }else{
-            mid = n/2;
-        }
+        
         int ans = 0;
         return Math.min(
-    Math.min(Math.max(mxleft, mnleft),
-             Math.max(mxright, mnright)),
-    Math.min(mxleft + mnright,
-             mnleft + mxright)
-);
-        
+                Math.min(Math.max(mxleft, mnleft),
+                        Math.max(mxright, mnright)),
+                Math.min(mxleft + mnright,
+                        mnleft + mxright));
+
     }
 }
