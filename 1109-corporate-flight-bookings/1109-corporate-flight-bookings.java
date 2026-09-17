a21412b197aaab6c0a2a9e32e3b54b0f169862de
subham saha <subham.saha.work01@gmail.com>
2026-09-18 01:24:52 +0530

    Time: 4 ms (36.01%), Space: 84.8 MB (29.55%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -1,14 +1,23 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int ans[] = new int[n];
+        int ans[] = new int[n+1];
+
         for(int i=0; i<bookings.length; i++){
             int l = bookings[i][0];
             int r = bookings[i][1];
             int val = bookings[i][2];
-            for(int idx=l-1; idx<r; idx++){
-                ans[idx] += val;
-            }
+            
+            // Start adding
+            ans[l-1] += val;
+            // Stop adding after r
+            ans[r] -= val;
         }
-        return ans;
+
+        // prefix sum
+        for(int i=1; i<n+1; i++){
+            ans[i] = ans[i] + ans[i-1];
+        }
+
+        return Arrays.copyOf(ans , n);
     }
 }
